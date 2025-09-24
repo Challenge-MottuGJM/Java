2d03777d4d620bf498765277d0d85254f3830515
Matheus de Freitas Silva <matheus.freitas_silva@outlook.com>
2025-09-24 09:45:17 -0300

    Removido classes de API Rest e Autenticação JWT e mantido apenas estrutuda do projeto para MVC

--- src/main/java/com/challenge/mottu/security/SegurancaConfig.java
+++ src/main/java/com/challenge/mottu/security/SegurancaConfig.java
@@ -1,56 +1,45 @@
 package com.challenge.mottu.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SegurancaConfig {
 	
-	@Autowired
-    private JWTAuthFilter jwtAuthFilter;
-	
 	@Bean
-    public SecurityFilterChain filtrar(HttpSecurity http) throws Exception {
+	public SecurityFilterChain filtrar(HttpSecurity http) throws Exception {
 
-        http
-            .csrf(csrf -> csrf.ignoringRequestMatchers("/api/**").disable()) 
-            .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+	    http.authorizeHttpRequests((request) ->
 
-            .authorizeHttpRequests(request -> request
-                .requestMatchers("/auth/login", "/api/**").permitAll()
+	        request.requestMatchers("/usuario/**")
+	            .hasAuthority("ADMINISTRADOR")
+	        .requestMatchers("/vaga/**", "/moto/**")
+	            .hasAnyAuthority("ADMINISTRADOR", "SUPERVISOR", "OPERADOR")
+	        .requestMatchers("/andar/**", "/bloco/**", "/galpao/**", "/patio/**")
+	            .hasAnyAuthority("ADMINISTRADOR", "SUPERVISOR")
+	        .anyRequest().authenticated())
+	    	.formLogin((login) -> login
+	        .loginPage("/login")
+	        .defaultSuccessUrl("/index", true)
+	        .failureUrl("/login?falha=true")
+	        .permitAll())
+	    	.logout((logout) -> logout
+	        .logoutUrl("/logout")
+	        .logoutSuccessUrl("/login?logout=true")
+	        .permitAll())
+	    	.exceptionHandling((exception) ->
+	        	exception.accessDeniedHandler((request, response, ex) -> {
+	        		response.sendRedirect("/acesso_negado");
+	        })
+	    );
 
-                // regras MVC com base nas roles conforme precisa
-                .requestMatchers("/usuario/**").hasAuthority("ADMINISTRADOR")
-                .requestMatchers("/vaga/**", "/moto/**").hasAnyAuthority("ADMINISTRADOR", "SUPERVISOR", "OPERADOR")
-                .requestMatchers("/andar/**", "/bloco/**", "/galpao/**", "/patio/**")
-                    .hasAnyAuthority("ADMINISTRADOR", "SUPERVISOR")
-                .anyRequest().authenticated())
-            .formLogin(form -> form
-                .loginPage("/login")
-                .defaultSuccessUrl("/index", true)
-                .failureUrl("/login?falha=true")
-                .permitAll())
-            .logout(logout -> logout
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/login?logout=true")
-                .permitAll())
-            .exceptionHandling(exception -> exception
-                .accessDeniedHandler((request, response, ex) -> response.sendRedirect("/acesso_negado")))
-            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-            .sessionManagement(session -> 
-                session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED));
-
-        return http.build();
+	    return http.build();
     }
 	
 	@Bean
